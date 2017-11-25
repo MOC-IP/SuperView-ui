dee0e32108864637d86f425025b07910eb06c29e
utilizatorvalid <vasilegroza3@gmail.com>
2017-11-25 09:17:10 +0200

    added home and 404 pagest to project via router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,
+      { enableTracing: true }// <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
